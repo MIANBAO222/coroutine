02f513266a69d32175fc9eec96c308bd4b2e255d
MIANBAO222 <756362171@qq.com>
2021-03-30 04:48:09 +0800

    Update main.c
    增加描述

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ foo(struct schedule * S, void *ud) {
 	int start = arg->n;
 	int i;
 	for (i=0;i<5;i++) {
-		printf("coroutine %d : %d\n",coroutine_running(S) , start + i);
-		coroutine_yield(S);
+		printf("coroutine %d : %d\n",coroutine_running(S) , start + i);//打印调度器此时正在running的协程id（即自己的协程id）
+		coroutine_yield(S);//协程主动放弃
 	}
 }
 
@@ -21,8 +21,8 @@ test(struct schedule *S) {
 	struct args arg1 = { 0 };
 	struct args arg2 = { 100 };
 
-	int co1 = coroutine_new(S, foo, &arg1);
-	int co2 = coroutine_new(S, foo, &arg2);
+	int co1 = coroutine_new(S, foo, &arg1);//创建协程1，此时未运行
+	int co2 = coroutine_new(S, foo, &arg2);//创建协程2，此时未运行
 	printf("main start\n");
 	while (coroutine_status(S,co1) && coroutine_status(S,co2)) {
 		coroutine_resume(S,co1);
